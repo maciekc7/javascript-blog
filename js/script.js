dfbe39d240bfe6ac4adf854f5508a78739c48fc8
maciekc7 <ciulkinmaciek7@gmail.com>
2021-10-14 18:26:10 +0200

    remove class 'active' from all articles

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
 'use strict';
 
-// function titleClickHandler(){
-//   const links = document.querySelectorAll('.titles a');
-//   console.log(links);
-// }
-
-// const buttonTest = document.getElementById('button-test');
-
-// buttonTest.addEventListener('click', titleClickHandler);
-
-// const links = document.querySelectorAll('.titles a');
-
-// for(let link of links){
-//   console.log(link);
-// }
-
-'use strict';
-
 function titleClickHandler(event){
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
+
   /* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.posts article.active')
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
